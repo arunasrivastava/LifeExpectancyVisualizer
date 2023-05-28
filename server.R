c44d9d99160927e77bff68642edaac6c4b42bdfa
Aruna Srivastava <aruna-sri@outlook.com>
2023-05-27 22:24:10 -0700

    shinyapp updates

--- server.R
+++ server.R
@@ -1,6 +1,16 @@
+library(shiny)
+library(plotly)
+library(ggplot2)
+library(tidyr)
+library(dplyr) 
+
+# le_df <- read.csv('data/le_database.csv', stringAsFactors = FALSE)
+
+
 # summary information 
 
 # server
 server <- function(input, output) {
 
 }
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -1,4 +1,13 @@
+library("plotly")
+library("shiny")
+library("shinythemes")
 
+ui <- fluidPage(
+  includeCSS("style.css"), 
+  theme = shinythemes::shinytheme("yeti"),
+  navbarPage("Life Expectancy Visualizations", tabPanel("Introduction"), tabPanel("V1"), tabPanel("V2"),tabPanel("V3"), tabPanel("Conclusion") )
+)
+shinyUI(ui)
 # page 1- introduction 
 
 # page 2- visual 1 
