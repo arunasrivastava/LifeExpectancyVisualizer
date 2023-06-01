1b37d0aba025a5133f8bd1103ee9d025d2e54d58
Aruna Srivastava <aruna-sri@outlook.com>
2023-05-31 22:26:15 -0700

    updates

--- server.R
+++ server.R
@@ -1,23 +1,134 @@
-library(shiny)
-library(plotly)
-library(ggplot2)
-library(tidyr)
-library(dplyr) 
+library("ggplot2")
+library("plotly")
+library("dplyr")
+library("styler")
+library("bslib")
+library("markdown")
+library("shiny") 
+library("tidyr")
 
-#le_df <- read.csv("data/le_database.csv", stringAsFactors = FALSE)
-data <- read.csv("C:/Users/knel2/Downloads/health_ineq_online_table_1 (1).csv", stringAsFactors = FALSE)
+#VIS 1 AND 2 DF
+health_df <- read.csv("data/health_ineq_online_table_income_percent_year.csv", stringsAsFactors = FALSE)
+
+#VIS 3 DF 
+data <- read.csv("data/le_database.csv", stringsAsFactors = FALSE)
+df <- read.csv("data/health_ineq_online_table_country_FM.csv", stringsAsFactors = FALSE)
+df <- df %>% 
+  mutate(country = gsub("United States of America", "USA", country)) %>%
+  mutate( country = gsub("Russian Federation", "Russia", country)) %>%
+  mutate(country = gsub("Saint Vincent and the Grenadines", "Grenadines", country)) %>%
+  mutate(country = gsub("Bolivia (Plurinational State of)", "Bolivia", country)) %>%
+  mutate(country = gsub("Venezuela (Bolivarian Republic of)", "Venezuela", country))
+map_data <- map_data("world")
+merged_data <- left_join(map_data, df, by = c( "region" = "country"))
+
+
+
 
 # summary information 
 
 # server
 server <- function(input, output) {
-  output$climate_graph <- renderPlotly(
-    data%>%group_by(gnd)%>%summarize(total_life_Expec = sum(le_agg)),
-    gnd_life_expectacy<- ggplot(data = changes_life_expentacy) +
-    geom_col(aes(x= gnd , y= total_life_Expec, fill = gnd)) + scale_color_brewer(palette = "Set3") +
-      
-    labs(title = "Trends of life Expentacy through gender", x = "Gender", y = "life expectancy"), 
-    ggplotly(gnd_life_expectacy)
-  )
+  #VIS 1 CODE 
+ output$lineplot <- renderPlotly({
+    health_df <- health_df %>% filter(pctile %in% input$cat_user)
+    health_df <- health_df %>% filter(year >= input$scale[1])
+    health_df <- health_df %>% filter(year <= input$scale[2])
+
+    # Filter and plot data for women (F)
+    women_df <- health_df %>% filter(gnd == "F")
+    lineplot <- ggplot(data = women_df) +
+      geom_line(mapping = aes(
+        x = year, 
+        y = hh_inc, 
+        color = "Women",
+        linetype = as.factor(pctile)
+      ), size = 1.2) +
+      labs(
+        title = "Annual Household Income For Men And Women 2001-2014",
+        x = "Year",
+        y = "Household income",
+        color = "Gender percentile" 
+      )
+
+    # Filter and plot data for men (M)
+    men_df <- health_df %>% filter(gnd == "M")
+    lineplot <- lineplot +
+      geom_line(data = men_df, mapping = aes(
+        x = year, 
+        y = hh_inc, 
+        color = "Men",
+        linetype = as.factor(pctile)
+      ), size = 1.2)
+
+    # Set custom color and linetype scales
+    lineplot <- lineplot +
+      scale_color_manual(values = c("Women" = "pink", "Men" = "blue")) +
+      scale_linetype_manual(values = c("1", "2", "3", "4"))  # Adjust linetypes as needed
+
+    lineplot
+})
+#VIS 2 CODE 
+ output$lineplot2 <- renderPlotly({
+    health_df <- health_df %>% filter(pctile %in% input$dog_user)
+    # health_df <- health_df %>% filter(year >= input$scale[1])
+    # health_df <- health_df %>% filter(year <= input$scale[2])
+
+    # Filter and plot data for women (F)
+    women_df <- health_df %>% filter(gnd == "F")
+    lineplot <- ggplot(data = women_df) +
+      geom_line(mapping = aes(
+        x = year, 
+        y = le_agg, 
+        color = "Women",
+        linetype = as.factor(pctile)
+      ), size = 1.2) +
+      labs(
+        title = "Life Expectancy For Men And Women 2001-2014",
+        x = "Year",
+        y = "Life Expectancy",
+        color = "Gender percentile" 
+      )
+
+    # Filter and plot data for men (M)
+    men_df <- health_df %>% filter(gnd == "M")
+    lineplot <- lineplot +
+      geom_line(data = men_df, mapping = aes(
+        x = year, 
+        y = le_agg, 
+        color = "Men",
+        linetype = as.factor(pctile)
+      ), size = 1.2)
+
+    # Set custom color and linetype scales
+    lineplot <- lineplot +
+      scale_color_manual(values = c("Women" = "pink", "Men" = "blue")) +
+      scale_linetype_manual(values = c("1", "2", "3", "4"))  # Adjust linetypes as needed
+
+    lineplot
+})
+#VIS 3 CODE
+output$map <- renderPlotly({
+    map <- ggplot(merged_data) +
+      geom_polygon(mapping = aes(x = long, 
+                                y = lat, 
+                                group = group,
+                                fill = !!as.name(input$gender),
+                                text = paste("Country: ", region, "<br>"))) + 
+      scale_fill_gradient(name = if (input$gender == "le_F") "Female Life Expectancy" 
+                                else "Male Life Expectancy", 
+                          low = "lightblue", 
+                          high = "darkblue", 
+                          na.value = "gray") +
+      coord_map() +
+      labs(title = if (input$gender == "le_F") "World Female Life Expectancy Map" 
+                  else "World Male Life Expectancy Map",
+           x = "Longitude",
+           y = "Latitude")
+    ggplotly(map, tooltip=c("text")) %>%
+      layout(showlegend = TRUE)
+    return(ggplotly(map))
+  })
+
 }
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -1,35 +1,118 @@
+library("ggplot2")
 library("plotly")
-library("shiny")
+library("dplyr")
+library("styler")
+library("bslib")
+library("markdown")
+library("shiny") 
 library("shinythemes")
 
+health_df <- read.csv("data/health_ineq_online_table_income_percent_year.csv", stringsAsFactors = FALSE)
+
+#VIS 1- widget 
+
+bar_plot <- sidebarPanel(
+  selectInput(
+    inputId = "cat_user",
+    label = "Income Percentile",
+    choices = unique(health_df$pctile),
+    selected = 50,
+    multiple = TRUE),
+  
+  sliderInput("scale", label = "Years", min = 2001, max = max(health_df$year), value = c(2001, 2014), sep = "", step = 1)
+)
+
+lineplot_main <- mainPanel(
+  plotlyOutput(outputId = "lineplot")
+)
+
+#VIS 2- widget 
+
+bar_plot2 <- sidebarPanel(
+  selectInput(
+    inputId = "dog_user",
+    label = "Income Percentile",
+    choices = unique(health_df$pctile),
+    selected = 50,
+    multiple = TRUE),
+
+    # sliderInput("scale", label = "Years", min = 2001, max = max(health_df$year), value = c(2001, 2014), sep = "", step = 1)
+
+)
+
+lineplot_main2 <- mainPanel(
+  plotlyOutput(outputId = "lineplot2")
+)
+ 
+ #UI
 ui <- fluidPage(
   includeCSS("style.css"), 
   theme = shinythemes::shinytheme("yeti"),
   navbarPage("Life Expectancy Visualizations", 
-  tabPanel("Introduction",
-         #img(id = "map-img", src = "https://www.clipartmax.com/png/middle/253-2533695_manage-all-health-monitoring-data-in-one-place-google-analytics-icons-free.png", alt = "map"),
-        mainPanel(
+  tabPanel("Introduction",  
+        img(id = "map-img", src = "people_map.jpg", alt = "map"),
+        mainPanel(class = "ctr", 
             h3(class = "title", "Overview"),
                  p("Our main question is how life expectancy changes across the U.S under factors like gender, race, and income distribution. 
                  This question is important in acknolwledging the disparities in the U.S and how they can impact the health of disadvantaged groups. "),
             h4(class = "title", "Selected Dataset"),
                  p("To address this we will analyze each of these factors individually and how they might be associated. 
                 The data used was collected and published by the", a(href = 'https://healthinequality.org/data/ ', "The Health Inequality Project", ".")),
-                 p("The health inequality project provides profound insight on the disparities of life expectancy in America based on gender and income. The data was collected from history from 1999 - 2014 from tax and social security records to record income data. They try to identify people by social security records in which they can identify where they are located and the size of their household. This data is collected by the groups from ages of 40 to 76. It is important to analyze this data and use the findings to address how everyone can have the equal opportunity to live healthy lives."),
+                 p("The health inequality project provides profound insight on the disparities of life expectancy in America based on gender and income. We will be using a dataset based upon income and gender in table 2 of the website. The data was collected from history from 1999 - 2014 from tax and social security records to record income data. They try to identify people by social security records in which they can identify where they are located and the size of their household. This data is collected by the groups from ages of 40 to 76. It is important to analyze this data and use the findings to address how everyone can have the equal opportunity to live healthy lives."),
              h4(class = "title", "Ethical Questions and Limitations"),
-                p("When looking at the data we can see the problem with the health inequality that the health system has over minority groups are reciving the help that they need. When we see the impact of the changes we have the question what is the impact of power that health systems have on people and how we can help solve this problem.
-                One aspect that we have noticed while examining the document regarding data collection methods is the exclusion of individuals without a tax return or social security record. This omission of information may lead to complications and raise questions regarding the future implementation of the dataset. By omitting such individuals, we fail to acquire essential data that is necessary for a comprehensive analysis. Additionally, the age range chosen, specifically from 40 to 70 years, raises some concerns as it primarily focuses on older individuals. It would be beneficial for our study to include individuals from various age groups to compare life expectancy and income, as this narrow age range may result in generalizations regarding life expectancy based on income."),
+                p("One aspect that we have noticed while examining the document regarding data collection methods is the exclusion of individuals without a tax return or social security record. This omission of information may lead to complications and raise questions regarding the future implementation of the dataset. By omitting such individuals, we fail to acquire essential data that is necessary for a comprehensive analysis. Additionally, the age range chosen, specifically from 40 to 70 years, raises some concerns as it primarily focuses on older individuals. It would be beneficial for our study to include individuals from various age groups to compare life expectancy and income, as this narrow age range may result in generalizations regarding life expectancy based on income. Additionally, the dataset is not pulled from the homeless population and may not reflect the reality of annual income for lower income percentiles in America"),
             h4(class = "title", "Questions"),
                  p(tags$ol(
-                     tags$li("Which gender tends to have a longer unadjusted life expectancy?"),
                      tags$li("Which gender tends to have a higher mean household income?"),
+                     tags$li("Which gender tends to have a longer unadjusted life expectancy?"),
                      tags$li("How does income correlate with life expectancy?")
                    )
                 )
         )
   ), 
-  tabPanel("V1"), tabPanel("V2"),tabPanel("V3"), tabPanel("Conclusion") )
+  tabPanel("Comparing Household Income",
+    "Visualization",
+     sidebarLayout(
+      bar_plot, #Widget
+      lineplot_main #Graph 
+     ),
+      h1("Insights"), 
+      p("Accross all income percentiles, it is clear that women make significantly less than men, especially at the median household income. Towards the extreme incomes, the top and bottom 10 percent, this pattern is not as evident as these individuals are not impacted by gender expectations nearly as much as those in the bulk of income earners. Additionally, we can see how annual household income has changed over time and the drops that have occurred. It is clear that inflation and other economic factors has caused a severe drop in annual household income for all income percentiles, with the exception of the top 5%. Additionally, we can see how the 2008 recession impacted different groups and how it is very clear that it was more impactful for those in the bottom 5 percent than the those in the 25th percentile.")
+  ), 
+  tabPanel("Comparing Life Expectancy", 
+    "Visualization",
+     sidebarLayout(
+      bar_plot2, #Widget
+      lineplot_main2 #Graph 
+     ),
+      h1("Insights"), 
+      p("Across all income percentiles, women live longer than men. This varies greatly accross different incomes in which we can see that those in the top 90% of income earners live upwards of 15 years longer than those in the bottom 10%. Although economical factors cannot be a sole predictor of longevity and life expectancy, it is clear that income plays a massive role in life expectancy in the U.S where healthcare and access to health education is highly dependent on income. Additionally, life expectancy seems to be on an upward trend for those in the upper percentile while more stagnant for those in the lower percentile. This could be attributed to the wealthy benefitting from advancements in healthcare and medicine while the poor are unable to gain access to these resources.")
+  
+  ),  
+  tabPanel("World Gender Analysis",
+      sidebarPanel(
+        radioButtons(
+          inputId = "gender",
+          label = h3("Which gender would you like to analyze?"),
+          choices = list("Female" = "le_F", "Male" = "le_M"),
+          selected = "le_F"
+        )
+      ),
+      mainPanel(
+        plotlyOutput(outputId = "map"))
+  ),
+  
+  tabPanel("Conclusion", 
+    h2("Takeaway 1: Gender disparities exist among the income-earning majorities in the U.S "), 
+    h4("From 2001-2014, the average household income for women is significantly less than men. This income disparity averages to around $10,830 and has the greatest disparity towards the upper percentile. Gender acts as a significant factor of household income for occupations that place individuals between 10-90% of income earners in the U.S. Towards the tail ends of income earners, gender acts as a less significant factor for income as these occupations are generally skewed by factors like generational wealth, or on the opposite end of the spectrum, inability to work or disabilities. Overall, the income disparity that existed in 2001 has not progressed and there needs to be greater advocacy for women to be paid more and receive the support to advocate for higher wages. "),
+    h2("Takeaway 2: Life expectancy for women is higher than for men across the U.S "), 
+    h4("Due to social, biological, and behavioral differences, women live upwards of 10 years longer than men. Similar to income, the differences in life expectancy seem to decline among the top and bottom household income earners in the U.S. It seems that this difference has been declining over time, which may be attributed to societal changes and improvements in healthcare for women. "),
+    h2("Takeaway 3: Life expectancy is associated with income in the U.S  "), 
+    h4("The difference in life expectancy is significantly influenced by income disparities. Individuals in the top 90% of income earners tend to live about 15 years longer than those in the bottom 10%. Although it's important to note that economic factors alone cannot determine longevity and life expectancy, it is evident that income plays a significant role in determining lifespan in the United States. This is because healthcare services and access to health education are largely dependent on income. Moreover, there is a noticeable trend of increasing life expectancy among the wealthy while it remains relatively stagnant for those with lower incomes. This disparity can be attributed to the fact that the affluent population benefits more from advancements in healthcare and medicine, while individuals with limited financial resources struggle to access these resources. From the trends we identified, we can see that the U.S needs to realize that capitalism has a detrimental impact on the poor such that their life expectancy is nearly 10 years less than the wealthy.")
+  ) )
 )
+
+
 shinyUI(ui)
 # page 1- introduction 
 
